2add09408e3bf4b96d82cceacbcc9271b112e16b
jeanport <pierreanatc522@gmail.com>
2025-03-26 14:23:55 -0300

    Criando dto ProfessorResponse

---
+++ src/main/java/com/jean/agendaprof/api/professores/dtos/ProfessorResponse.java
@@ -0,0 +1,33 @@
+package com.jean.agendaprof.api.professores.dtos;
+
+import jakarta.persistence.Column;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ProfessorResponse {
+
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String idade;
+
+    private String descricao;
+
+    private BigDecimal valorHora;
+
+    private String fotoPerfil;
+
+    private String password;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
