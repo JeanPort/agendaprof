f7a92b4d04eac84dbc737bb86865f9f6ac81d209
jeanport <pierreanatc522@gmail.com>
2025-03-26 14:31:09 -0300

    Adicionando metodo findAllByDescriptionContaining no ProfessorRepository

--- src/main/java/com/jean/agendaprof/core/repositories/ProfessorRepository.java
+++ src/main/java/com/jean/agendaprof/core/repositories/ProfessorRepository.java
@@ -3,5 +3,9 @@ package com.jean.agendaprof.core.repositories;
 import com.jean.agendaprof.core.models.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+    List<Professor> findAllByDescriptionContaining(String q);
 }
