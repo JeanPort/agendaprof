57a1b2681d2df4f2e287c80274f00b0b7f2199e5
jeanport <pierreanatc522@gmail.com>
2025-03-26 10:21:32 -0300

    Iniciando o projeto

---
+++ src/main/java/com/jean/agendaprof/AgendaprofApplication.java
@@ -0,0 +1,13 @@
+package com.jean.agendaprof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaprofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaprofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jean/agendaprof/AgendaprofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jean.agendaprof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaprofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
