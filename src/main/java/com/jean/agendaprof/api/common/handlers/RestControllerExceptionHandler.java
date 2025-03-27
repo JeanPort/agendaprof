16bdf28daff93602a8756098f9f90a9979a68f81
jeanport <pierreanatc522@gmail.com>
2025-03-27 17:49:58 -0300

    Criando tratamento da excecao EmailAlreadyInUseException

--- src/main/java/com/jean/agendaprof/api/common/handlers/RestControllerExceptionHandler.java
+++ src/main/java/com/jean/agendaprof/api/common/handlers/RestControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.jean.agendaprof.api.common.handlers;
 
 import com.jean.agendaprof.api.common.dtos.ErrorResponse;
+import com.jean.agendaprof.core.exceptions.EmailAlreadyInUseException;
 import com.jean.agendaprof.core.exceptions.ModelNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,19 @@ public class RestControllerExceptionHandler {
     @ExceptionHandler(ModelNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleModelNotFoundException(ModelNotFoundException e){
         var status = HttpStatus.NOT_FOUND;
+        var error = ErrorResponse.builder()
+                .status(status.value())
+                .message(e.getLocalizedMessage())
+                .timestamp(LocalDateTime.now())
+                .error(status.getReasonPhrase())
+                .cause(e.getClass().getSimpleName())
+                .build();
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(EmailAlreadyInUseException.class)
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyInUseException(EmailAlreadyInUseException e){
+        var status = HttpStatus.BAD_REQUEST;
         var error = ErrorResponse.builder()
                 .status(status.value())
                 .message(e.getLocalizedMessage())
