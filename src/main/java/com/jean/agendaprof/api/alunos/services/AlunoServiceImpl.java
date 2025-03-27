8f328e1b1d3493a2a0bef61d4c29a0a7f5129cf9
jeanport <pierreanatc522@gmail.com>
2025-03-27 17:47:49 -0300

    Criando metodo para validar email na classe AlunoServiceImpl

--- src/main/java/com/jean/agendaprof/api/alunos/services/AlunoServiceImpl.java
+++ src/main/java/com/jean/agendaprof/api/alunos/services/AlunoServiceImpl.java
@@ -3,6 +3,7 @@ package com.jean.agendaprof.api.alunos.services;
 import com.jean.agendaprof.api.alunos.dtos.AlunoRequest;
 import com.jean.agendaprof.api.alunos.dtos.AlunoResponse;
 import com.jean.agendaprof.api.alunos.mappers.AlunoMapper;
+import com.jean.agendaprof.core.exceptions.EmailAlreadyInUseException;
 import com.jean.agendaprof.core.exceptions.ProfessorNotFoundException;
 import com.jean.agendaprof.core.repositories.AlunoRepository;
 import com.jean.agendaprof.core.repositories.ProfessorRepository;
@@ -21,11 +22,18 @@ public class AlunoServiceImpl implements AlunoService{
     public AlunoResponse insert(AlunoRequest alunoRequest, Long professorId) {
 
         var professor = professorRepository.findById(professorId).orElseThrow(ProfessorNotFoundException::new);
+        emailValidator(alunoRequest.getEmail());
         var aluno = alunoMapper.toAluno(alunoRequest);
         aluno.setProfessor(professor);
 
         var alunoCadastrado = alunoRepository.save(aluno);
 
         return alunoMapper.toAlunoResponse(alunoCadastrado);
+    }
+
+    private void emailValidator(String email) {
+        if( alunoRepository.existsByEmail(email)){
+            throw new EmailAlreadyInUseException();
+        }
     }
 }
