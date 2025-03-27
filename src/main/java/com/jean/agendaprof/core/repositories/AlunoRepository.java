43ed525c3bd87af84ad621d5dc55c71b61969f52
jeanport <pierreanatc522@gmail.com>
2025-03-27 17:19:02 -0300

    Criando AlunoRepository

---
+++ src/main/java/com/jean/agendaprof/core/repositories/AlunoRepository.java
@@ -0,0 +1,7 @@
+package com.jean.agendaprof.core.repositories;
+
+import com.jean.agendaprof.core.models.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
