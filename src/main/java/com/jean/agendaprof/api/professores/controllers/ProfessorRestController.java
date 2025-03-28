b5e250c4e0652b3a3babdd772bececf47640774a
jeanport <pierreanatc522@gmail.com>
2025-03-28 09:25:20 -0300

    Implementando metodo insert no ProfessorRestController

--- src/main/java/com/jean/agendaprof/api/professores/controllers/ProfessorRestController.java
+++ src/main/java/com/jean/agendaprof/api/professores/controllers/ProfessorRestController.java
@@ -1,14 +1,14 @@
 package com.jean.agendaprof.api.professores.controllers;
 
 import com.jean.agendaprof.api.common.routes.ApiRoutes;
+import com.jean.agendaprof.api.professores.dtos.ProfessorRequest;
 import com.jean.agendaprof.api.professores.dtos.ProfessorResponse;
 import com.jean.agendaprof.api.professores.services.ProfessorService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +28,12 @@ public class ProfessorRestController {
     public ResponseEntity<ProfessorResponse> findAll(@PathVariable Long professorId){
         var res = professorService.findById(professorId);
         return ResponseEntity.ok(res);
+    }
+
+    @PutMapping(ApiRoutes.CADASTRO_PROFESSOR)
+    public ResponseEntity<ProfessorResponse> insert(@Valid @RequestBody ProfessorRequest professorRequest){
+        var result = professorService.insert(professorRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
 }
