16b75f750f04bca06d09dcf1256179f485a1e815
jeanport <pierreanatc522@gmail.com>
2025-03-31 08:56:17 -0300

    Criando classe AuthenticationService

--- src/main/java/com/jean/agendaprof/core/repositories/ProfessorRepository.java
+++ src/main/java/com/jean/agendaprof/core/repositories/ProfessorRepository.java
@@ -4,9 +4,11 @@ import com.jean.agendaprof.core.models.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 
     List<Professor> findAllByDescricaoContaining(String q);
     boolean existsByEmail(String email);
+    Optional<Professor> findByEmail(String email);
 }
---
+++ src/main/java/com/jean/agendaprof/core/service/AuthenticationService.java
@@ -0,0 +1,23 @@
+package com.jean.agendaprof.core.service;
+
+import com.jean.agendaprof.core.exceptions.ProfessorNotFoundException;
+import com.jean.agendaprof.core.models.AuthenticatedUser;
+import com.jean.agendaprof.core.repositories.ProfessorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService implements UserDetailsService {
+
+    private final ProfessorRepository professorRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var professor = professorRepository.findByEmail(username).orElseThrow(() ->new UsernameNotFoundException("Credenciais invalidas"));
+        return new AuthenticatedUser(professor);
+    }
+}
