59893ab35468def808fa2bab46a702a82cf5336c
jeanport <pierreanatc522@gmail.com>
2025-04-01 14:14:11 -0300

    Criando classe LoginResponse

--- src/main/java/com/jean/agendaprof/api/auth/dtos/LoginRequest.java
+++ src/main/java/com/jean/agendaprof/api/auth/dtos/LoginRequest.java
@@ -1,6 +1,9 @@
 package com.jean.agendaprof.api.auth.dtos;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -8,12 +11,15 @@ import lombok.Data;
 
 @Data
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class LoginRequest {
 
     @NotNull
     @Email
+    @NotEmpty
     private String email;
     @NotNull
+    @NotEmpty
     @Size(min = 6, max = 20)
     private String password;
 }
---
+++ src/main/java/com/jean/agendaprof/api/auth/dtos/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.jean.agendaprof.api.auth.dtos;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class LoginResponse {
+
+    private String token;
+    private String refreshToken;
+}
