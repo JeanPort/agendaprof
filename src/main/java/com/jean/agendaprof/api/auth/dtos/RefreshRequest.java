edc71ffc640cd7a5688b332c4a626785989edcf6
jeanport <pierreanatc522@gmail.com>
2025-04-02 09:13:55 -0300

    Criando a classe RefreshRequest

---
+++ src/main/java/com/jean/agendaprof/api/auth/dtos/RefreshRequest.java
@@ -0,0 +1,14 @@
+package com.jean.agendaprof.api.auth.dtos;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class RefreshRequest {
+
+    private String refreshToken;
+}
